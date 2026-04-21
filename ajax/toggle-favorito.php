6d1de1336238570c0173e8bd36a4f5d9af71f34d
Jorge Martinez Vegara <jormarveg@alu.edu.gva.es>
2026-04-21 20:26:00 +0200

    Ficha de juego con acciones rapidas y favoritos AJAX

---
+++ ajax/toggle-favorito.php
@@ -0,0 +1,51 @@
+<?php
+require '../api/cache.php';
+require '../includes/auth.php';
+
+header('Content-Type: application/json; charset=UTF-8');
+
+if (!estaLogueado()) {
+    http_response_code(403);
+    echo json_encode([
+        'ok' => false,
+        'mensaje' => 'Debes iniciar sesion'
+    ]);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode([
+        'ok' => false,
+        'mensaje' => 'Metodo no permitido'
+    ]);
+    exit;
+}
+
+$idVideojuego = (int) ($_POST['id_videojuego'] ?? 0);
+$favorito = isset($_POST['favorito']) ? (int) $_POST['favorito'] : null;
+
+if ($idVideojuego <= 0 || ($favorito !== 0 && $favorito !== 1)) {
+    http_response_code(422);
+    echo json_encode([
+        'ok' => false,
+        'mensaje' => 'Datos no validos'
+    ]);
+    exit;
+}
+
+$actualizado = cacheActualizarFavoritoJuegoBiblioteca($db, (int) getUsuario()['id'], $idVideojuego, $favorito === 1);
+
+if (!$actualizado) {
+    http_response_code(500);
+    echo json_encode([
+        'ok' => false,
+        'mensaje' => 'No se ha podido actualizar el favorito'
+    ]);
+    exit;
+}
+
+echo json_encode([
+    'ok' => true,
+    'favorito' => $favorito === 1
+]);
